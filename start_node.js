a344a58c0e3e7425d3c802f1d3da6131a73f492f
Julien DERAY <julien@deray.fr>
2015-06-30 08:59:50 +0200

    moves startup script out of gossip folder

---
+++ start_node.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+var advertisement = require('./gossip/core/advertisement');
+var view = require('./visualisation/index');
+var gossipManager = require('./gossip/app/gossip_manager');
+
+var ip = require("ip");
+const nodeIp = ip.address();
+
+var nodeLaunched = false;
+
+const nodeName = require('./gossip/core/name_picker').getNodeName();
+console.log('>>> node name is :', nodeName);
+
+advertisement.start(nodeName);
+searchNodeAndConnect();
+
+view.startHttpServer(nodeIp, gossipManager);
+gossipManager.setView(view);
+
+function searchNodeAndConnect() {
+    advertisement.searchOneNode(function (service) {
+        if ( service.addresses.indexOf( nodeIp ) < 0 && !nodeLaunched) {
+            console.log('Node found :');
+            console.log('   IP :', service.addresses);
+            console.log('   Host :', service.host);
+
+            console.log('Stop looking for nodes');
+            advertisement.stopSearching();
+
+            startNode(service.addresses[0]);
+        }
+    });
+}
+
+function startNode(peer_ip) {
+    const nodePort = 9000;
+    var peerAddr = [peer_ip, nodePort].join(':');
+
+    console.log('Connecting to node', peerAddr);
+
+    const localNodeInfos = {
+        ip : nodeIp,
+        port : nodePort,
+        name : nodeName
+    };
+
+    gossipManager.start(localNodeInfos, peerAddr, function() {
+        nodeLaunched = true;
+        console.log('Node', localNodeInfos.ip, '(' + nodeName + ')', 'started');
+    });
+}
