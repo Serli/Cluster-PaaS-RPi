124d2cea2894ee58112bd67bdd8d452f78522d30
Julien DERAY <julien@deray.fr>
2015-07-03 10:04:27 +0200

    adds tr command to remove brackets in the output of nmap

--- utils/shell_utils.sh
+++ utils/shell_utils.sh
@@ -1,5 +1,5 @@
 # trouver les pi du réseau
-sudo nmap -sP 192.168.86.0/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
+sudo nmap -sP 192.168.86.0/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}' | tr -d '()'
 
 # installer une raspbian
 sudo dd bs=1m if=/Users/julienderay/Serli/Cluster-RPI/2015-02-16-raspbian-wheezy.img | pv | sudo dd of=/dev/disk2
