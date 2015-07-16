68260dc86314bf948cfee878c345d1cac4d30a89
Julien DERAY <julien@deray.fr>
2015-07-16 13:34:16 +0200

    adds the mac command that unmounts the sd card

--- utils/misc/shell_utils.sh
+++ utils/misc/shell_utils.sh
@@ -2,4 +2,5 @@
 sudo nmap -sP 192.168.86.0/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}' | tr -d '()'
 
 # Install a Rapsian on a SD card
+diskutil unmountDisk /dev/disk2
 sudo dd bs=1m if=/Users/julienderay/Serli/Cluster-RPI/2015-02-16-raspbian-wheezy.img | pv | sudo dd of=/dev/disk2
